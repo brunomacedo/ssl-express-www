6d3ad84ff5e8aa91f6f412e081a022b9c976700b
Bruno Macedo <contato@brunomacedo.com.br>
2018-04-29 18:58:02 -0300

    [SPEC] Add suport to localhost

--- specs/ForceSSL.spec.js
+++ specs/ForceSSL.spec.js
@@ -2,11 +2,30 @@ import { expect } from 'chai';
 import ForceSSL from '../source/ForceSSL';
 
 describe('Package Express Force HTTPS', () => {
+  it('Should do nothing when localhost is required', () => {
+    const req = {
+      url: '/app/',
+      headers: {
+        'x-forwarded-proto': 'http',
+        host: 'localhost:3000',
+      },
+    };
+    let next = () => `${req.headers['x-forwarded-proto']}://${req.headers.host}${req.url}`;
+    const res = {
+      redirect(callback) {
+        next = callback;
+        return next;
+      },
+    };
+    ForceSSL(req, res, next);
+    expect(res.redirect(next())).to.be.equal('http://localhost:3000/app/');
+  });
+
   it('Should force schema to HTTPS when HTTP is require', () => {
     const req = {
       url: '/',
       headers: {
-        'x-forwarded-proto': '',
+        'x-forwarded-proto': 'http',
         host: 'example.com.br',
       },
     };
@@ -25,7 +44,7 @@ describe('Package Express Force HTTPS', () => {
     const req = {
       url: '/',
       headers: {
-        'x-forwarded-proto': '',
+        'x-forwarded-proto': 'http',
         host: 'www.example.com.br',
       },
     };
