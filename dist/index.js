9f37727e8b20b6f18df9d50eb15e97f3beae17cf
Bruno Macedo <contato@brunomacedo.com.br>
2018-04-29 18:55:25 -0300

    Build dist umd file

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,9 @@
 /*!
- * ssl-express-www v1.0.20
+ * ssl-express-www v1.3.0
  * git+https://github.com/brunomacedo/ssl-express-www.git
  * License MIT © Bruno Macedo
  */
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.sslExpress = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.sslExpress = f()}})(function(){var define,module,exports;return (function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
@@ -19,15 +19,19 @@ exports.default = function (req, res, next) {
     return site.replace(/\/$/, '');
   };
 
-  if (www.indexOf('localhost') < 0 && schema !== 'https') {
+  var notLocalHost = function notLocalHost() {
+    return www.indexOf('localhost') < 0;
+  };
+
+  if (notLocalHost() && schema !== 'https') {
     res.redirect(removeSlash(fullUrl));
-  } else if (/^www\./i.test(req.headers.host) && schema === 'https') {
+  } else if (notLocalHost() && /^www\./i.test(req.headers.host) && schema === 'https') {
     res.redirect(removeSlash(fullUrl));
-  } else if (/\/$/.test(fullUrl) && fullUrl !== 'https://' + www + '/') {
+  } else if (notLocalHost() && /\/$/.test(fullUrl) && fullUrl !== 'https://' + www + '/') {
     res.redirect(removeSlash(fullUrl));
-  } else {
-    next();
   }
+
+  return next();
 };
 
 module.exports = exports['default'];
