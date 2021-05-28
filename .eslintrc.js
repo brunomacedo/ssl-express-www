5c84e63c9b6a016e94c7e6cddd0c4f4c6aadf0f5
Russell Kerns <russell.kerns@foragerscs.com>
2021-05-28 11:43:59 -0500

    added sinon, added prettier config, updated test, and made sure not to call next when redirecting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
-  "extends": "airbnb",
-  "env": {
-    "mocha": true
+  extends: 'airbnb',
+  env: {
+    mocha: true
+  },
+  rules: {
+    'trailing-commas': ignore
   }
 };
--- specs/ForceSSL.spec.js
+++ specs/ForceSSL.spec.js
@@ -1,81 +1,70 @@
 import { expect } from 'chai';
+import { spy } from 'sinon';
 import ForceSSL from '../source/ForceSSL';
 
 describe('Package Express Force HTTPS', () => {
-  it('Should do nothing when localhost is required', () => {
-    const req = {
+  let req;
+  let res;
+  let next;
+  beforeEach(() => {
+    req = {
       url: '/app',
       headers: {
         'x-forwarded-proto': 'http',
-        host: 'localhost:3000',
-      },
+        host: 'localhost:3000'
+      }
     };
-    let next = () => `${req.headers['x-forwarded-proto']}://${req.headers.host}${req.url}`;
-    const res = {
-      redirect(callback) {
-        next = callback;
-        return next;
-      },
+    res = {
+      redirect: spy()
     };
+
+    next = spy();
+  });
+
+  it('Should do nothing when localhost is required', () => {
     ForceSSL(req, res, next);
-    expect(res.redirect(next())).to.be.equal('http://localhost:3000/app');
+    expect(next.called).to.equal(true);
   });
 
   it('Should force schema to HTTPS when HTTP is require', () => {
-    const req = {
+    req = {
       url: '/',
       headers: {
         'x-forwarded-proto': 'http',
-        host: 'example.com.br',
-      },
-    };
-    let next = () => `${req.headers['x-forwarded-proto']}://${req.headers.host}${req.url}`;
-    const res = {
-      redirect(callback) {
-        next = callback;
-        return next;
-      },
+        host: 'example.com.br'
+      }
     };
     ForceSSL(req, res, next);
-    expect(res.redirect(next)).to.be.equal('https://example.com.br');
+    expect(res.redirect.calledWith('https://example.com.br')).to.equal(true);
+    expect(next.called).to.equal(false);
   });
 
   it('Should remove www and forces HTTPS', () => {
-    const req = {
+    req = {
       url: '/',
       headers: {
         'x-forwarded-proto': 'http',
-        host: 'www.example.com.br',
-      },
+        host: 'www.example.com.br'
+      }
     };
-    let next = () => `${req.headers['x-forwarded-proto']}://${req.headers.host}${req.url}`;
-    const res = {
-      redirect(callback) {
-        next = callback;
-        return next;
-      },
-    };
+
     ForceSSL(req, res, next);
-    expect(res.redirect(next)).to.be.equal('https://example.com.br');
+    expect(res.redirect.calledWith('https://example.com.br')).to.equal(true);
+    expect(next.called).to.equal(false);
   });
 
   it('Should remove trailing slash when path is require', () => {
-    const req = {
+    req = {
       url: '/app/',
       headers: {
         'x-forwarded-proto': 'https',
-        host: 'example.com.br',
-      },
-    };
-
-    let next = () => `${req.headers['x-forwarded-proto']}://${req.headers.host}${req.url}`;
-    const res = {
-      redirect(callback) {
-        next = callback;
-        return next;
-      },
+        host: 'example.com.br'
+      }
     };
     ForceSSL(req, res, next);
-    expect(res.redirect(next)).to.be.equal('https://example.com.br/app');
+    expect(res.redirect.calledWith('https://example.com.br/app')).to.equal(
+      true
+    );
+    expect(next.called).to.equal(false);
   });
 });
